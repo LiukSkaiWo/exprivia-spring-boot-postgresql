765bc6cc42ec94298b161de6d68a010aa8a84b6c
LiukSkaiWo <albergo.vargroup@gmail.com>
2023-08-03 16:05:49 +0200

    Updated code

---
+++ src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.accessingdatamysql.User;
+
+import java.util.List;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+
+    List<User> findByName(String name);
+
+    List<User> findUserByNameIgnoreCase(String name);
+}
